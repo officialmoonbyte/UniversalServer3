dba62a287c0c3a1880dc49553c236a3f1180370f
alexritter@moonbyte.net <alexritter@moonbyte.net>
2019-12-11 16:40:41 -0600

    test appupdate - can't communicate to server

--- UniversalServer/UniversalClientTest/Program.cs
+++ UniversalServer/UniversalClientTest/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Universalclient;
 
 namespace UniversalClientTest
 {
@@ -10,6 +11,9 @@ namespace UniversalClientTest
     {
         static void Main(string[] args)
         {
+            UniversalClient client = new UniversalClient(true);
+            client.ConnectToRemoteServer("192.168.0.2", 7876);
+            client.SendCommand("testing", new string[] { "test", "123" });
         }
     }
 }
