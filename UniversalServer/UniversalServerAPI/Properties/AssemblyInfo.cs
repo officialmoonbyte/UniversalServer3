3701cca6ae621ce1cd21d29ee52acaa129ee56d4
Alex Ritter <alexritter@moonbyte.net>
2020-08-19 13:53:07 -0500

    *Updated plugin version

--- UniversalServer/UniversalServerAPI/Properties/AssemblyInfo.cs
+++ UniversalServer/UniversalServerAPI/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Moonbyte Corporation")]
 [assembly: AssemblyProduct("UniversalServerAPI")]
-[assembly: AssemblyCopyright("Copyright © UniversalServer 2019")]
+[assembly: AssemblyCopyright("Copyright © Moonbyte Corporation 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.1.2.0")]
-[assembly: AssemblyFileVersion("3.1.2.0")]
+[assembly: AssemblyVersion("4.2.3.0")]
+[assembly: AssemblyFileVersion("4.2.3.0")]
